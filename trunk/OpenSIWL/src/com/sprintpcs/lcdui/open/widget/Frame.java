d3df7f18ebce46d8161d5b97355e55561fb5ef3a
mjumbewu <mjumbewu@5caf2a94-fa33-11dd-99c0-75d572ba1ddd>
2009-02-14 01:37:41 +0000

    First commit

---
+++ trunk/OpenSIWL/src/com/sprintpcs/lcdui/open/widget/Frame.java
@@ -0,0 +1,76 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.sprintpcs.lcdui.open.widget;
+
+import javax.microedition.lcdui.Image;
+
+/**
+ *
+ * @author mjumbewu
+ */
+public abstract class Frame {
+    /**
+     * Constant for the stock light background image
+     */
+    public static final int BACKGROUND_LIGHT = 0;
+
+    /**
+     * Constant for the stock dark background image
+     */
+    public static final int BACKGROUND_DARK = 1;
+
+    /**
+     * Constant for white-filled background
+     */
+    public static final int BACKGROUND_NONE = 2;
+
+    private int m_backgroundColor = 0x00ffffff;
+    private Image m_backgroundImage = null;
+
+    /**
+     * Set the background color for the frame.
+     * @param aColor The new background color of the frame in the form
+     *               0x00rrggbb.
+     */
+    public void setBackgroundColor(int aColor) {
+        m_backgroundColor = aColor;
+    }
+
+    /**
+     * Get the background color of the frame.  By default, this value is
+     * 0x00ffffff (white)
+     * @return The background color of the frame in the format 0x00rrggbb
+     */
+    public int getBackgroundColor() {
+        return m_backgroundColor;
+    }
+
+    /**
+     * Set the background image for the frame.
+     * @param aImage The new image.  If <code>null</code>, this will clear the
+     *               background image.
+     * @see clearBackgroundImage()
+     */
+    public void setBackgroundImage(Image aImage) {
+        m_backgroundImage = aImage;
+    }
+
+    /**
+     * Get the background image for the frame.  By default, this value is null.
+     * @return The background image of the frame.
+     */
+    public Image getBackgroundImage() {
+        return m_backgroundImage;
+    }
+
+    /**
+     * Clears the background image from the frame.  The background of the frame
+     * will be flooded with the background color.
+     */
+    public void clearBackgroundImage() {
+        m_backgroundImage = null;
+    }
+}
