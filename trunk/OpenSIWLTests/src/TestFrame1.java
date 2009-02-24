84532bcd0e1cdaeb8ff1298876249eb395a88f30
mjumbewu <mjumbewu@5caf2a94-fa33-11dd-99c0-75d572ba1ddd>
2009-02-24 08:18:39 +0000

    Tests for the OpenSIWL

---
+++ trunk/OpenSIWLTests/src/TestFrame1.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import oiwl.widget.BasicFrame;
+
+/**
+ *
+ * @author mjumbewu
+ */
+public class TestFrame1 extends BasicFrame {
+    
+}
