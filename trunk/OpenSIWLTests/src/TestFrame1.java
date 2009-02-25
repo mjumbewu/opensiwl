0b96e132cdafe515b4897c07e713fb02593dd5c7
mjumbewu <mjumbewu@5caf2a94-fa33-11dd-99c0-75d572ba1ddd>
2009-02-25 17:16:37 +0000

    wrote simple parent class for TestFrames

--- trunk/OpenSIWLTests/src/TestFrame1.java
+++ trunk/OpenSIWLTests/src/TestFrame1.java
@@ -3,12 +3,25 @@
  * and open the template in the editor.
  */
 
-import oiwl.widget.BasicFrame;
+import oiwl.widget.*;
 
 /**
  *
  * @author mjumbewu
  */
-public class TestFrame1 extends BasicFrame {
-    
+public class TestFrame1 extends Frame {
+    public TestFrame1() {
+        super(Orientation.PORTRAIT);
+        Layout layout = new VLinearLayout();
+        this.setLayout(layout);
+        layout.manage(new StaticText("This is a simple test.  It  "));
+        layout.manage(new StaticText("consists of a Frame with a  "));
+        layout.manage(new StaticText("vertical linear layout      "));
+        layout.manage(new StaticText("manager.  Several StaticText"));
+        layout.manage(new StaticText("objects are managed within  "));
+        layout.manage(new StaticText("the layout.  They should be "));
+        layout.manage(new StaticText("displayed in a column on the"));
+        layout.manage(new StaticText("screen.  The default        "));
+        layout.manage(new StaticText("expansion is true.  "));
+    }
 }
