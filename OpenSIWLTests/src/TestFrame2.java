e26bad7312c521a5eb686bfcb39e45394767a692
mjumbewu <mjumbewu@5caf2a94-fa33-11dd-99c0-75d572ba1ddd>
2009-02-27 14:06:19 +0000

    - Rearrange text in second test to not be as wide  - Use getGlobalX/YPos instead of getX/YPos to test correctness of Widget placement relative to each other

--- OpenSIWLTests/src/TestFrame2.java
+++ OpenSIWLTests/src/TestFrame2.java
@@ -11,8 +11,8 @@ import oiwl.widget.*;
  */
 public class TestFrame2 extends TestFrame {
     Layout layout = new LinearLayout(Orientation.VERTICAL);
-    StaticText st1 = new StaticText("This Frame starts off with two StaticText");
-    StaticText st2 = new StaticText("Widgets in a vertical LinearLayout.");
+    StaticText st1 = new StaticText("This one starts off with two");
+    StaticText st2 = new StaticText("text items arranged vertically.");
     
     StaticText st_new1 = new StaticText("Bottom1");
     StaticText st_new2 = new StaticText("Bottom2");
@@ -26,10 +26,11 @@ public class TestFrame2 extends TestFrame {
         this.setLayout(layout);
         layout.manage(st1);
         layout.manage(st2);
-        layout.manage(new StaticText("Then two more are added at the bottom."));
-        layout.manage(new StaticText("Finally, two more are inserted within"));
-        layout.manage(new StaticText("a horizontally oriented LinearLayout"));
-        layout.manage(new StaticText("across all of these."));
+        layout.manage(new StaticText("Then two more are added at the"));
+        layout.manage(new StaticText(" bottom.  Finally, two more are "));
+        layout.manage(new StaticText("inserted within a horizontally "));
+        layout.manage(new StaticText("oriented LinearLayout across "));
+        layout.manage(new StaticText("all of these."));
     }
     
     protected boolean runtest() {
@@ -40,13 +41,13 @@ public class TestFrame2 extends TestFrame {
         hor_new.manage(st_hor1);
         hor_new.manage(st_hor2);
         
-        return (st1.getLocalYPos() < st2.getLocalYPos() &&
-                st2.getLocalYPos() < hor_new.getLocalYPos() &&
-                st2.getLocalYPos() < st_hor1.getLocalYPos() &&
-                st_hor1.getLocalYPos() == st_hor2.getLocalYPos() &&
-                st_hor2.getLocalYPos() < st_new1.getLocalYPos() &&
-                hor_new.getLocalYPos() < st_new1.getLocalYPos() &&
-                st_new1.getLocalYPos() < st_new2.getLocalYPos() &&
-                st_hor1.getLocalXPos() < st_hor2.getLocalXPos());
+        return (st1.getGlobalYPos() < st2.getGlobalYPos() &&
+                st2.getGlobalYPos() < hor_new.getGlobalYPos() &&
+                st2.getGlobalYPos() < st_hor1.getGlobalYPos() &&
+                st_hor1.getGlobalYPos() == st_hor2.getGlobalYPos() &&
+                st_hor2.getGlobalYPos() < st_new1.getGlobalYPos() &&
+                hor_new.getGlobalYPos() < st_new1.getGlobalYPos() &&
+                st_new1.getGlobalYPos() < st_new2.getGlobalYPos() &&
+                st_hor1.getGlobalXPos() < st_hor2.getGlobalXPos());
     }
 }
