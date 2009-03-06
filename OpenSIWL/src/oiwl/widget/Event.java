8a1b01e6f325eeaa3222176481775e054baf4c5d
mjumbewu <mjumbewu@5caf2a94-fa33-11dd-99c0-75d572ba1ddd>
2009-03-06 22:52:12 +0000

    Event.java:  - Moved TapEvent and DoubleTapEvent into Event.java file  - Separated out state machine functionality from event into EventMachine

--- OpenSIWL/src/oiwl/widget/Event.java
+++ OpenSIWL/src/oiwl/widget/Event.java
@@ -5,20 +5,10 @@
 
 package oiwl.widget;
 
+import java.util.Date;
+
 /**
- * An object that stores event state/progress.  An Event object is essentially
- * a finite state machine.  Different events move it from one state to the next.
- * For example, a PushButton click is implemented in three states: START, 
- * HOLDING, and ACCEPT.  
- * <ul>
- * <li>From the START state, a PRESSED event in the sending Widget will move the
- * click Event object to the HOLDING state</li>
- * <li>From the HOLDING state, a DRAGGED event outside of the sending Widget
- * will move the click Event back to the START state.</li>
- * <li>From the HOLDING state, a RELEASED event inside of the sending Widget
- * will move the click Event to the ACCEPT state.</li>
- * </ul>
- * Double click would be implemented in much the same way.
+ * 
  * @author mjumbewu
  */
 public class Event {
@@ -31,6 +21,7 @@ public class Event {
     public static final int DRAGGED = NewEventType();
     public static final int RELEASED = NewEventType();
     public static final int FLICKED = NewEventType();
+    public static final int TIME_EXPIRED = NewEventType();
     
     public static final int TAPPED = NewEventType();
     public static final int CLICKED = TAPPED;
@@ -61,7 +52,25 @@ public class Event {
      */
     public static final int STATE_CHANGED = NewEventType();
     
-    
+}
+
+/**
+ * An object that stores event state/progress.  An Event object is essentially
+ * a finite state machine.  Different events move it from one state to the next.
+ * For example, a PushButton click is implemented in three states: START,
+ * HOLDING, and ACCEPT.
+ * <ul>
+ * <li>From the START state, a PRESSED event in the sending Widget will move the
+ * click Event object to the HOLDING state</li>
+ * <li>From the HOLDING state, a DRAGGED event outside of the sending Widget
+ * will move the click Event back to the START state.</li>
+ * <li>From the HOLDING state, a RELEASED event inside of the sending Widget
+ * will move the click Event to the ACCEPT state.</li>
+ * </ul>
+ * Double click would be implemented in much the same way.
+ * @author mjumbewu
+ */
+class EventMachine {
     private static int st_id = 1;
     protected static int NewEventState() {
         return st_id++;
@@ -72,14 +81,12 @@ public class Event {
     
     int m_eventType;
     int m_eventState = START_STATE;
-    Widget m_eventSender;
     
-    public Event(int type, Widget sender) {
+    public EventMachine(int type) {
         this.m_eventType = type;
-        this.m_eventSender = sender;
     }
     
-    public int getType() {
+    public int getEventType() {
         return this.m_eventType;
     }
     
@@ -87,23 +94,122 @@ public class Event {
         return this.m_eventState;
     }
     
-    public Widget getSender() {
-        return this.m_eventSender;
-    }
-    
     protected void gotoState(int state) {
         m_eventState = state;
     }
     
-    public void resetEvent() {
+    public void reset() {
         m_eventState = START_STATE;
     }
     
-    public void acceptEvent() {
+    public void accept() {
         m_eventState = ACCEPT_STATE;
     }
-    
+
     public void advance(int type, Object data) {
         gotoState(ACCEPT_STATE);
+    }
+}
+
+
+abstract class TargetedEventMachine extends EventMachine {
+    Widget m_target;
+
+    public TargetedEventMachine(int type, Widget target) {
+        super(type);
+        m_target = target;
+    }
+
+    public Widget getTarget() {
+        return this.m_target;
+    }
+
+}
+class TapEventMachine extends TargetedEventMachine {
+    public static final int HOLDING_STATE = NewEventState();
+
+    public TapEventMachine(Widget target) {
+        super(Event.TAPPED, target);
+    }
+
+    public void advance(int type, Object data) {
+        int state = this.getState();
+        Widget target = getTarget();
+
+        if (state == START_STATE) {
+            if (type == Event.PRESSED) {
+                LocationData pressPoint = (LocationData)data;
+                if (target.containsGlobal(pressPoint.x, pressPoint.y)) {
+                    gotoState(HOLDING_STATE);
+                    target.redraw();
+                }
+            }
+        }
+
+        else if (state == HOLDING_STATE) {
+            // If the pointer is dragged outside of te button, cancel any
+            // pending tapEvent events.
+            if (type == Event.DRAGGED) {
+                LocationData dragPoint = (LocationData)data;
+                if (!target.containsGlobal(dragPoint.x, dragPoint.y)) {
+                    reset();
+                    target.redraw();
+                }
+            }
+
+            // When the user releases the pointer inside the button, send a tapEvent
+            // event if there is one pending.  If the release is outside of the
+            // button then cancel any pending tapEvent events.
+            else if (type == Event.RELEASED) {
+                LocationData releasePoint = (LocationData)data;
+                if (target.containsGlobal(releasePoint.x, releasePoint.y)) {
+                    accept();
+                } else {
+                    reset();
+                }
+                target.redraw();
+            }
+        }
+    }
+}
+
+class DoubleTapEventMachine extends TargetedEventMachine {
+    private long dtap_time;
+    public static final int TAPPED_ONCE_STATE = NewEventState();
+
+    public DoubleTapEventMachine(Widget target, long dtap_time) {
+        super(Event.DTAPPED, target);
+        this.dtap_time = dtap_time;
+    }
+
+    private long first_tap_time;
+    public void advance(int type, Object data) {
+        int state = this.getState();
+
+        if (state == START_STATE) {
+            if (type == Event.TAPPED) {
+                first_tap_time = (new Date()).getTime();
+                gotoState(TAPPED_ONCE_STATE);
+            }
+        }
+
+        else if (state == TAPPED_ONCE_STATE) {
+            if (type == Event.TAPPED) {
+                long tap_time = (new Date()).getTime();
+                System.out.println(tap_time - first_tap_time);
+                if (tap_time - first_tap_time <= dtap_time) {
+                    accept();
+                } else {
+                    first_tap_time = (new Date()).getTime();
+                    gotoState(TAPPED_ONCE_STATE);
+                }
+            }
+        }
+    }
+}
+
+class TimeoutEventMachine extends EventMachine {
+    public TimeoutEventMachine(long duration) {
+        super(Event.TIME_EXPIRED);
     }
 }
--- OpenSIWL/src/oiwl/widget/PushButton.java
+++ OpenSIWL/src/oiwl/widget/PushButton.java
@@ -6,91 +6,6 @@
 package oiwl.widget;
 
 import javax.microedition.lcdui.Graphics;
-import java.util.Date;
-
-class TapEvent extends Event {
-    public static final int HOLDING_STATE = NewEventState();
-    
-    public TapEvent(Widget sender) {
-        super(Event.TAPPED, sender);
-    }
-    
-    public void advance(int type, Object data) {
-        int state = this.getState();
-        Widget sender = getSender();
-        
-        if (state == START_STATE) {
-            if (type == Event.PRESSED) {
-                LocationData pressPoint = (LocationData)data;
-                if (sender.containsGlobal(pressPoint.x, pressPoint.y)) {
-                    gotoState(HOLDING_STATE);
-                    sender.redraw();
-                }
-            }
-        }
-        
-        else if (state == HOLDING_STATE) {
-            // If the pointer is dragged outside of te button, cancel any 
-            // pending tapEvent events.
-            if (type == Event.DRAGGED) {
-                LocationData dragPoint = (LocationData)data;
-                if (!sender.containsGlobal(dragPoint.x, dragPoint.y)) {
-                    resetEvent();
-                    sender.redraw();
-                }
-            }
-            
-            // When the user releases the pointer inside the button, send a tapEvent 
-            // event if there is one pending.  If the release is outside of the 
-            // button then cancel any pending tapEvent events.
-            else if (type == Event.RELEASED) {
-                LocationData releasePoint = (LocationData)data;
-                if (sender.containsGlobal(releasePoint.x, releasePoint.y)) {
-                    acceptEvent();
-                } else {
-                    resetEvent();
-                }
-                sender.redraw();
-            }
-        }
-    }
-}
-
-class DoubleTapEvent extends Event {
-    private long dtap_time;
-    public static final int TAPPED_ONCE_STATE = NewEventState();
-    
-    public DoubleTapEvent(Widget sender, long dtap_time) {
-        super(Event.DTAPPED, sender);
-        this.dtap_time = dtap_time;
-    }
-    
-    private long first_tap_time;
-    public void advance(int type, Object data) {
-        int state = this.getState();
-        
-        if (state == START_STATE) {
-            if (type == Event.TAPPED) {
-                first_tap_time = (new Date()).getTime();
-                gotoState(TAPPED_ONCE_STATE);
-            }
-        }
-        
-        else if (state == TAPPED_ONCE_STATE) {
-            if (type == Event.TAPPED) {
-                long tap_time = (new Date()).getTime();
-                System.out.println(tap_time - first_tap_time);
-                if (tap_time - first_tap_time <= dtap_time) {
-                    acceptEvent();
-                } else {
-                    first_tap_time = (new Date()).getTime();
-                    gotoState(TAPPED_ONCE_STATE);
-                }
-            }
-        }
-    }
-}
-
 /**
  *
  * @author mjumbewu
@@ -125,9 +40,10 @@ public class PushButton extends Button {
     protected void onTapped() {}
     
     public void doTap() {
-        this.getEventSender().sendEvent(Event.TAPPED, this, null);
-        this.handleEvent(Event.TAPPED, null);
-        this.onTapped();
+        if (!this.handleEvent(Event.TAPPED, null)) {
+            this.getEventSender().sendEvent(Event.TAPPED, this, null);
+            this.onTapped();
+        }
     }
     
     public void doDTap() {
@@ -135,20 +51,20 @@ public class PushButton extends Button {
         this.handleEvent(Event.DTAPPED, null);
     }
     
-    TapEvent tapEvent = new TapEvent(this);
-    DoubleTapEvent dtapEvent = new DoubleTapEvent(this, 300);
+    TapEventMachine tapEvent = new TapEventMachine(this);
+    DoubleTapEventMachine dtapEvent = new DoubleTapEventMachine(this, 300);
     
     public boolean handleEvent(int type, Object data) {
         tapEvent.advance(type, data);
-        if (tapEvent.getState() == TapEvent.ACCEPT_STATE) {
-            this.tapEvent.resetEvent();
+        if (tapEvent.getState() == TapEventMachine.ACCEPT_STATE) {
+            this.tapEvent.reset();
             this.doTap();
             return true;
         }
         
         dtapEvent.advance(type, data);
-        if (dtapEvent.getState() == DoubleTapEvent.ACCEPT_STATE) {
-            this.dtapEvent.resetEvent();
+        if (dtapEvent.getState() == DoubleTapEventMachine.ACCEPT_STATE) {
+            this.dtapEvent.reset();
             return true;
         }
         
@@ -157,7 +73,7 @@ public class PushButton extends Button {
     
     public void draw(Graphics g, int xoff, int yoff, int x, int y, int w, int h) {
         int c0 = g.getColor();
-        if (tapEvent.getState() != TapEvent.HOLDING_STATE)
+        if (tapEvent.getState() != TapEventMachine.HOLDING_STATE)
             g.setColor(0x0000ff00);
         else
             g.setColor(0x000000ff);
